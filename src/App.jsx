1bde585c55b97583366265d2e070413363e4aa66
julombe <jose.ulombe@hotmail.com>
2021-03-18 17:56:05 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 
-import { Counter } from './components/Counter';
 import { RepositoryLIst } from './components/RepositoryLIst';
 import './styles/global.scss'; // importa o arquivo css
 
 export function App() {
     return (
-        <>
             <RepositoryLIst />
-            <Counter />
-        </>
     )
 }
