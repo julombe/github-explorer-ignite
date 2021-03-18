22ff3b16fc89a65e07adb5c441b9fe9dc3cbde61
julombe <jose.ulombe@hotmail.com>
2021-03-18 15:49:21 +0100

    initial commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,14 @@
 export function Counter() {
+
+    let counter = 0;
     function increment() {
         console.log('Incrementing');
     }
     return(
         <div>
-            <h2>0</h2>
-            <button type="button">Increment</button>
+            <h2>{counter}</h2>
+            <button type="button" onClick={increment}>
+                Increment</button>
         </div>
     );
 }
